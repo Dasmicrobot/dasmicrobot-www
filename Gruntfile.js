3d4e8ad320808d8c0d030a68cbbea83ec8a684c2
ivarprudnikov <ivar.prudnikov@gmail.com>
2018-08-08 23:58:17 +0100

    add webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,152 +1,8 @@
-'use strict';
-
-var path = require('path');
-
-// configurable paths
-var yeomanConfig = {
-  app: 'app',
-  dist: 'dist'
-};
-
 module.exports = function (grunt) {
 
-  require('load-grunt-tasks')(grunt);
-  require('time-grunt')(grunt);
+  grunt.loadNpmTasks('grunt-aws');
 
   grunt.initConfig({
-
-    yeoman: yeomanConfig,
-
-    clean: {
-      dist: {
-        files: [
-          {
-            dot: true,
-            src: [ '.tmp', '<%= yeoman.dist %>' ]
-          }
-        ]
-      }
-    },
-
-    eslint: {
-      all: [
-        'Gruntfile.js',
-        '<%= yeoman.app %>/{,**/}*.js'
-      ]
-    },
-
-    useminPrepare: {
-      html: '<%= yeoman.app %>/index.html',
-      options: {
-        srcStrip: '<%= yeoman.app %>',
-        dest: '<%= yeoman.dist %>',
-        flow: {
-          steps: {
-            js: ['concat'],
-            css: ['concat', 'cssmin']
-          },
-          post: {}
-        }
-      }
-    },
-
-    cssmin: {
-      options: {
-        aggressiveMerging: true,
-        keepBreaks: true
-      },
-      dist: {
-        files: [
-          {
-            expand: true,
-            cwd: '.tmp',
-            src: '**/*.css',
-            dest: '<%= yeoman.dist %>'
-          }
-        ]
-      }
-    },
-
-    htmlmin: {
-      dist: {
-        options: {},
-        files: [
-          {
-            expand: true,
-            cwd: '<%= yeoman.app %>',
-            src: ['{,*/}*.html'],
-            dest: '<%= yeoman.dist %>'
-          }
-        ]
-      }
-    },
-
-    // Put files not handled in other tasks here
-    ///////////////////////////////////////////////
-    copy: {
-      dist: {
-        files: [
-          {
-            expand: true,
-            dot: true,
-            cwd: '<%= yeoman.app %>',
-            dest: '<%= yeoman.dist %>',
-            src: [ '*.{ico,png,svg,txt}' ]
-          }
-        ]
-      },
-      styles: {
-        expand: true,
-        cwd: '<%= yeoman.merged %>',
-        dest: '.tmp/',
-        src: '**/*.css'
-      }
-
-    },
-
-    // uglify js files
-    ///////////////////////////////////////
-    uglify: {
-      options: {
-        sourceMap: true,
-        mangle: {},
-        compress: {},
-        beautify: false,
-        banner: '/* Time: <%= yeoman.timestamp %> */'
-      },
-      dist: {
-        files: [
-          {
-            expand: true,
-            cwd: '<%= yeoman.dist %>',
-            src: '**/*.js',
-            dest: '<%= yeoman.dist %>'
-          }
-        ]
-      }
-    },
-
-    // hash files
-    ///////////////////////
-    filerev: {
-      options: {},
-      dist: {
-        src: [
-          '<%= yeoman.dist %>/**/*.js',
-          '<%= yeoman.dist %>/**/*.css',
-          '<%= yeoman.dist %>/_shared/fonts/*.{css,otf,eot,ttf,woff,svg}',
-          '<%= yeoman.dist %>/**/*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      }
-    },
-
-    usemin: {
-      html: ['<%= yeoman.dist %>/{,**/}*.html'],
-      css: ['<%= yeoman.dist %>/**/*.css'],
-      options: {
-        dirs: ['<%= yeoman.dist %>']
-      }
-    },
 
     s3: {
       options: {
@@ -161,7 +17,7 @@ module.exports = function (grunt) {
             CacheControl: 1296000
           }
         },
-        cwd: '<%= yeoman.dist %>',
+        cwd: 'dist',
         src: ['**', '!**/*.html'] // everything except html files
       },
       html: {
@@ -170,25 +26,12 @@ module.exports = function (grunt) {
             CacheControl: 'no-cache, no-store, max-age=0, must-revalidate'
           }
         },
-        cwd: '<%= yeoman.dist %>',
+        cwd: 'dist',
         src: '**/*.html'
       }
     }
 
   });
-
-  grunt.registerTask('build', [
-    'clean',
-    'eslint',           // code quality check
-    'useminPrepare',    // prepares build steps for html blocks
-    'concat',           // concatinates files and moves them to dist
-    'cssmin',           // minifies css in tmp and moves to dist
-    'htmlmin',          // minifies html and moves them to dist
-    'copy:dist',        // copies previously unhandled files to dist
-    'uglify',           // minifies & uglifies js files in dist
-    'filerev',          // hashes js/css/img/font files in dist
-    'usemin'
-  ]);
 
   grunt.registerTask('deploy', function () {
     // Usage: grunt deploy --bucket=<bucket> [--region=<region>]
@@ -200,9 +43,4 @@ module.exports = function (grunt) {
       's3:html'
     ]);
   });
-
-  grunt.registerTask('default', [
-    'clean',
-    'build'
-  ]);
 };
---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+require('normalize.css/normalize.css');
+require('../css/main.css');
+require('../css/home.css');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("DOMContentLoaded")
+
+});
