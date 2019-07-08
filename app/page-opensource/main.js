8fedf3b90caff944ed18f701fa6ecb155bf2356e
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-07-08 11:11:52 +0300

    New 'Open source' page

---
+++ app/page-opensource/main.js
@@ -0,0 +1,11 @@
+require('../css/theme.min.css');
+require('../css/header.css');
+window.jQuery = require('jquery');
+require('bootstrap/dist/js/bootstrap.bundle');
+require('../js/theme.min.js');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("DOMContentLoaded", "page-opensource")
+
+});
