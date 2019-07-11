ee4b7cdfc26abc48a84b99cd56fabdaa0482af28
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-07-11 19:17:23 +0100

    Add popper.js and initialize show/hide plugin

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,13 @@
 require('../css/theme.min.css');
 require('../css/main.css');
 require('../css/header.css');
-window.jQuery = require('jquery');
-require('bootstrap/dist/js/bootstrap.bundle');
+window.jQuery = window.$ = require('jquery');
+window.Popper = require('popper.js/dist/umd/popper.js');
+require('bootstrap/dist/js/bootstrap.bundle.js');
 require('../js/theme.min.js');
+
+document.addEventListener("DOMContentLoaded", () => {
+  // initialization of show animations
+  window.$.HSCore.components.HSShowAnimation.init('.js-animation-link');
+});
+
