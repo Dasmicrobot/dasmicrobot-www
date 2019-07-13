ea042d67cd8efd189a91aa2e15ce9a565f0de91a
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-07-13 20:59:46 +0100

    Initialize SVGInjector

--- app/js/index.js
+++ app/js/index.js
@@ -3,11 +3,13 @@ require('../css/main.css');
 require('../css/header.css');
 window.jQuery = window.$ = require('jquery');
 window.Popper = require('popper.js/dist/umd/popper.js');
+window.SVGInjector = require('svg-injector')
 require('bootstrap/dist/js/bootstrap.bundle.js');
 require('../js/theme.min.js');
 
 document.addEventListener("DOMContentLoaded", () => {
   // initialization of show animations
   window.$.HSCore.components.HSShowAnimation.init('.js-animation-link');
+  window.$.HSCore.components.HSSVGIngector.init('.js-svg-injector');
 });
 
